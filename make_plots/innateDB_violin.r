71d095cdcd62ece3db5eae3a7d3b984d0623633e
Kimberly Olney <olneykimberly@gmail.com>
2020-08-04 16:07:00 -0700

    font size in plots

--- make_plots/innateDB_violin.r
+++ make_plots/innateDB_violin.r
@@ -23,8 +23,9 @@ rbind.all.columns <- function(x, y) {
 df_merged <- rbind.all.columns(df_merged_placenta, df_merged_pisarska)
 
 innateDB_inter <- intersect(innateDB$Geneid, df_merged$Geneid)
-genes <- c("XIST", "DDX3X", "CXCL10")
-
+genes <- c("XIST", "DDX3X", "CXCL10", "CXCL9", "EGR1", "MSR1", "NLRCA")
+typeof(genes)
+genes2 <- as.character(up_female$Geneid)
 #placenta_gene_PLOTS <- "./FIGURES/ViolinJitters/innateDB_genes.pdf"
 #pdf(placenta_gene_PLOTS)
 
@@ -35,12 +36,19 @@ violoin_Func <- function(a) {
   means <- aggregate(value ~group+sex, geneDF, mean)
   p <- ggplot(geneDF, aes(x = sex, y = value, color = sex)) +
     geom_violin() + scale_color_manual(values = c("black", "#66C2A5",  "#FC8D62")) +
-    facet_wrap(~group) +
+    facet_wrap(~group) + theme(strip.text.x = element_text(size = 12)) +
     geom_boxplot(width = 0.1, outlier.shape = NA) + 
     geom_jitter(aes(shape = factor(sex)),
                 size = 3,
                 position = position_jitter(0.1)) +
     theme(legend.position = "none") + ggtitle(paste0(a, " lcpm")) +
+    theme(axis.title.x=element_text(size=15), 
+          axis.text.x=element_text(size=12)) +
+    theme(axis.title.y=element_text(size=15),
+          axis.text.y=element_text(size=12)) +
+    theme(axis.title=element_text(size=15)) +
+    theme(legend.text=element_text(size=12)) +
+    theme(legend.title=element_text(size=15)) +
     geom_text(
       data = means,
       aes(
@@ -60,10 +68,6 @@ violoin_Func <- function(a) {
 
 # Map: iterates through items in Meta (which is a list of dataframes)
 # and iterates through the names of the items in Meta simultaneously
-violinPlots <- Map(violoin_Func, a = genes)
-violinPlots$CXCL10
-
-
-dev.off()
-dev.off()
+violinPlots <- Map(violoin_Func, a = genes2)
+violinPlots$DDX3X
 
